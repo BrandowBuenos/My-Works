38b517203991fa593f42eded97a154a553a4c194
Your_Name <Your-email@mail.com>
2020-08-09 16:25:58 -0300

    feat: new layout

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -1,95 +1,272 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Grid } from '@material-ui/core';
+import { Grid, Avatar, Typography } from '@material-ui/core';
 
-import { FaGithub, FaLinkedin } from 'react-icons/fa';
+import { FaGithub, FaLinkedin, FaDribbble, FaBehance } from 'react-icons/fa';
 
-import Background from "../../assets/img/png/background.png";
+import Background from "../../components/Background";
 
 export default function Main() {
 
+  const vh = window.innerHeight * 0.01
+  document.documentElement.style.setProperty('--vh', `${vh}px`)
+
+  window.addEventListener('resize', () => {
+    const vh = window.innerHeight * 0.01
+    document.documentElement.style.setProperty('--vh', `${vh}px`)
+  })
+
   const Container = styled(Grid)`
-    background-color: #F1F1F1;
-    background-image: url(${Background});
-    background-size: cover;
-    background-repeat: no-repeat;
-    height:100vh;
-    font-size: larger;
-  `;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh auto;
+  height: calc(var(--vh, 1vh) * 100);
+`
+  const UserDetails = styled(Grid)`
+  display: flex;
+  justify-content: center;
+  height: 100%;
+  background: rgba(255, 255, 255, 0.7);
+  backdrop-filter: blur(10px);
 
-  const Card = styled(Grid)`
-    height: 100vh;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-  `;
+  .avatar {
+    width: 5em;
+    height: 5em;
+  }
 
-  const ProfilePictureContent = styled(Grid)`
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    height: 20vh;
+  @media only screen and (max-width: 1280px) {
+    background-color: transparent;
+  }
+`
+  const ProfilePicture = styled(Grid)`
+  display: flex;
+  align-items: flex-end;
+  justify-content: center;
+  height: 20%;
+`
+  const UserName = styled(Grid)`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 10%;
+`
+  const Status = styled(Grid)`
+  display: flex;
+  align-items: flex-start;
+  justify-content: center;
+  height: 10%;
+  text-align: center;
+`
+  const About = styled(Grid)`
+  display: flex;
+  align-items: flex-start;
+  justify-content: flex-start;
+  height: 25%;
+`
 
-    img {
-      border-radius:50%;
-      width:15vh;
+const Skills = styled(Grid)`
+  display: flex;
+  align-items: flex-start;
+  justify-content: flex-start;
+  height: 35%;
+`
+  const Main = styled(Grid)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+  const Content = styled(Grid)`
+  height: 100% auto;
+  display: flex;
+  align-items: center;
+  
+  .band {
+    width: 100%;
+    margin: 0 auto;
+    margin-bottom: 1em;
+    
+    display: grid;
+    
+    grid-template-columns: 1fr;
+    grid-template-rows: auto;
+    grid-gap: 20px;
+
+  }
+  
+  @media only screen and (min-width: 500px) {
+    .band {
+      grid-template-columns: 1fr 1fr;
+    }  
+  }
+  
+  @media only screen and (min-width: 850px) {
+    .band {
+      grid-template-columns: 1fr 1fr 1fr 1fr;
     }
-  `;
-
-  const UserDescription = styled(Grid)`
-    display: flex;
-    align-items: flex-start;
-    height: 10vh;
-  `;
-
-  const SocialNetworks = styled(Grid)`
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    height:10vh;
-  `;
-
-  const Networks = styled(Grid)`
-    display: flex;
-    align-items: center;
-    justify-content: center;
-  `;
-
-  const Link = styled.a`
+  }
+  
+  .card {
+    background: rgba(255, 255, 255, 0.9);
+    backdrop-filter: blur(10px);
     text-decoration: none;
-    color: #333;
-    font-size:20vh;
+    color: #444;
+    box-shadow: 0 2px 5px rgba(0,0,0,0.1);
     display: flex;
-    align-items: center;
-    justify-content: center;
-  `;
+    flex-direction: column;
+    min-height: 80%;
+  
+    border-radius: 10px;
+    
+    position: relative;
+    top: 0;
+    transition: all .1s ease-in;
+  }
+  
+  .card:hover {
+    top: -2px;
+    box-shadow: 0 4px 5px rgba(0,0,0,0.2);
+  }
+  
+  .card article {
+    padding: 20px;
+  }
+  
+  .card .thumb {
+    padding-bottom: 60%;
+    background-size: cover;
+    background-position: center center;
+  }
+  
+  .card h1 {
+    font-size: 20px;
+    margin-bottom: 1vh;
+    color: #009688;
+  }
+  
+  .card p { 
+    line-height: 1.4;
+  }
+  
+  .card span {
+    font-size: 12px;
+    font-weight: bold;
+    color: #999;
+    text-transform: uppercase;
+    letter-spacing: .05em;
+    margin: 2em 0 0 0;
+  }
 
+  .card article {
+    padding: 20px;
+    flex: 1;
+     
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+  }
+
+  .card p { 
+    flex: 1;
+    line-height: 1.4;
+  }
+`
 
   return (
     <>
-      <Container item xs={12} sm={12} md={12} lg={12}>
-        <Card container item xs={12} sm={12} md={12} lg={12}>
-          <ProfilePictureContent item xs={11} sm={2} md={2} lg={2}>
-            <img src='https://avatars3.githubusercontent.com/u/52222495?s=400&u=bc8eea80b72ed91315f15410654712b8ad20285a&v=4' alt="" />
-          </ProfilePictureContent>
-          <UserDescription container item xs={11} sm={6} md={4} lg={4}>
-            <div>
-              <i>Brandow Buenos Aires Madeira</i><br />
-              <h4>
-                Graduating in Software Engineering at Federal University of Pampa
-               </h4>
-            </div>
-          </UserDescription>
+      <Background />
+      <Container container item xs={12} sm={12} md={12} lg={12}>
 
-          <SocialNetworks container item xs={11} sm={11} md={11} lg={4}>
-            <Link href="https://github.com/BrandowBuenos"><Networks item xs={2} sm={2} md={2} lg={2}><FaGithub /></Networks></Link>
-            <Link href="https://www.linkedin.com/in/brandowbuenos/"><Networks item xs={2} sm={2} md={2} lg={2}><FaLinkedin /></Networks></Link>
-          </SocialNetworks>
-        </Card>
+        <UserDetails container item xs={12} sm={12} md={12} lg={3}>
+          <ProfilePicture container item xs={11} sm={11} md={11} lg={11}>
+            <Avatar alt="Brandow Buenos" className="avatar" src="https://avatars3.githubusercontent.com/u/52222495?s=400&u=bc8eea80b72ed91315f15410654712b8ad20285a&v=4" />
+          </ProfilePicture>
+          <UserName container item xs={11} sm={11} md={11} lg={11}>
+            <i>Brandow Buenos</i>
+          </UserName>
+          <Status container item xs={11} sm={11} md={11} lg={11}>
+            <h4>
+              Graduating in Software Engineering <br />at Federal University of Pampa
+          </h4>
+          </Status>
+
+          <About container item xs={11} sm={11} md={11} lg={11}>
+            <div>
+              <Typography variant="overline" display="block" gutterBottom>
+                About
+              </Typography>
+              <Typography variant="body1" gutterBottom>
+                <p>Use things, love people. Not the other way around.</p>
+                <p> Hello world!</p>
+                <p>I am Software Engineering Student from Universidade Federal do Pampa and eternal Full Stack apprentice.</p>
+              </Typography>
+            </div>
+          </About>
+
+          <Skills container item xs={11} sm={11} md={11} lg={11}>
+            <div>
+              <Typography variant="overline" display="block" gutterBottom>
+                Skills
+              </Typography>
+              <Typography variant="body1" gutterBottom>
+                <p>UI / UX</p>
+                <p>Design</p>
+                <p>Java</p>
+                <p>Javascript (ReactJS)</p>
+                <p>HTML</p>
+                <p>CSS</p>
+                <p>Git</p>
+                <p>CSS</p>
+              </Typography>
+              </div>
+          </Skills>
+        </UserDetails>
+
+        <Main container item xs={11} sm={11} md={11} lg={9}>
+          <Content container item xs={12} sm={12} md={12} lg={11}>
+            <div className="band">
+              <div className="item-1">
+                <a href="https://github.com/BrandowBuenos" className="card">
+                  <article>
+                    <h1 style={{ color: "rgb(27,31,34)" }}>Github</h1>
+                    <p>Code Jobs</p>
+                    <span><FaGithub /></span>
+                  </article>
+                </a>
+              </div>
+              <div className="item-2">
+                <a href="https://www.linkedin.com/in/brandowbuenos" className="card">
+                  <article>
+                    <h1 style={{ color: "rgb(12,98,166)" }}>Linkedin</h1>
+                    <p>Business profile</p>
+                    <span><FaLinkedin /></span>
+                  </article>
+                </a>
+              </div>
+              <div className="item-3">
+                <a href="https://dribbble.com/BrandowBuenos" className="card">
+                  <article>
+                    <h1 style={{ color: "#ea4c89" }}>Dribbble</h1>
+                    <p>UI/UX Projects</p>
+                    <span><FaDribbble /></span>
+                  </article>
+                </a>
+              </div>
+              <div className="item-4">
+                <a href="https://www.behance.net/brandowbuenos/" className="card">
+                  <article>
+                    <h1 style={{ color: "#2b2b2b" }}>Behance</h1>
+                    <p>UI/UX Projects</p>
+                    <span><FaBehance /></span>
+                  </article>
+                </a>
+              </div>
+            </div>
+          </Content>
+        </Main>
       </Container>
+
     </>
   );
 }
 
-var availableScrolHeight = document.body.scrollHeight;
-window.scrollTo(0, availableScrolHeight);
